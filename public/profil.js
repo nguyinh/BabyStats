15c5ba96ffd040601edf7fac7a05bfc828109425
Nguyinh <florent.dupont@edu.esiee.fr>
2018-02-21 17:47:11 +0100

    Rectified "profil" CSS and Beautify a bit

--- public/profil.js
+++ public/profil.js
@@ -1,15 +1,14 @@
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
-document.getElementById("affiche_confirm").style.display ="none"
+document.getElementById("affiche_confirm").style.display = "none"
 
-$('input[type=radio]').change( function() {
-   var signin = document.getElementById('signin').checked;
-   if(signin == false){
-     document.getElementById("affiche_confirm").style.display ="block";
-   }
-   else {
-     document.getElementById("affiche_confirm").style.display ="none";
-   }
+$('input[type=radio]').change(function() {
+    var signin = document.getElementById('signin').checked;
+    if (signin == false) {
+        document.getElementById("affiche_confirm").style.display = "block";
+    } else {
+        document.getElementById("affiche_confirm").style.display = "none";
+    }
 });
 
 // Button add player listener
@@ -28,54 +27,53 @@ document.getElementById("log_in").addEventListener("click", function() {
     //SIGN IN
     if (id != "" && password != "" && signin == true) {
         firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-      });
-    }
-    else if (id != "" && password != ""  && signin == false) {     // if inputs are not empty and the password is not the same of password confirm
-        // AUTH ici gros
-        if(password == password_confirm){
-          firebase.auth().createUserWithEmailAndPassword(id, password).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
-            document.getElementById("error_info").style.display ="block";
-            document.getElementById("error_message").style.display ="block";
-            $("#error_message").removeClass("text-success");
-            $("#error_message").addClass("text-danger");
-            var errortxt = document.getElementById("error_message");
-            errortxt.textContent = errorMessage;
-            console.log(errorCode);
-            if (errorCode == "auth/invalid-email"){
-              $("#ID_input").addClass("is-invalid");
-            }
-            if (errorCode == "auth/weak-password"){
-              $("#password_input").addClass("is-invalid");
-              $("#password_confirm_input").addClass("is-invalid");
-            }});
-            document.getElementById("error_info").style.display ="block";
-            document.getElementById("error_message").style.display ="block";
+            // ...
+        });
+    } else if (id != "" && password != "" && signin == false) { // if inputs are not empty and the password is not the same of password confirm
+        // AUTH ici gros
+        if (password == password_confirm) {
+            firebase.auth().createUserWithEmailAndPassword(id, password).catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                document.getElementById("error_info").style.display = "block";
+                document.getElementById("error_message").style.display = "block";
+                $("#error_message").removeClass("text-success");
+                $("#error_message").addClass("text-danger");
+                var errortxt = document.getElementById("error_message");
+                errortxt.textContent = errorMessage;
+                console.log(errorCode);
+                if (errorCode == "auth/invalid-email") {
+                    $("#ID_input").addClass("is-invalid");
+                }
+                if (errorCode == "auth/weak-password") {
+                    $("#password_input").addClass("is-invalid");
+                    $("#password_confirm_input").addClass("is-invalid");
+                }
+            });
+            document.getElementById("error_info").style.display = "block";
+            document.getElementById("error_message").style.display = "block";
             $("#error_message").removeClass("text-danger");
             $("#error_message").addClass("text-success");
             var errortxt = document.getElementById("error_message");
             errortxt.textContent = "Successfull profil creation.";
-        }
-        else {
-            document.getElementById("error_info").style.display ="block";
-            document.getElementById("error_message").style.display ="block";
+        } else {
+            document.getElementById("error_info").style.display = "block";
+            document.getElementById("error_message").style.display = "block";
             var errortxt = document.getElementById("error_message");
             errortxt.textContent = "Password and Password confirmation must be the same.";
             $("#password_input").addClass("is-invalid");
             $("#password_confirm_input").addClass("is-invalid");
-        }}
-    else {
-    if (id == "")
-        $("#ID_input").addClass("is-invalid");
-    if (password == "")
-        $("#password_input").addClass("is-invalid");
-    if (password == "")
-        $("#password_confirm_input").addClass("is-invalid");
+        }
+    } else {
+        if (id == "")
+            $("#ID_input").addClass("is-invalid");
+        if (password == "")
+            $("#password_input").addClass("is-invalid");
+        if (password == "")
+            $("#password_confirm_input").addClass("is-invalid");
     }
 });
