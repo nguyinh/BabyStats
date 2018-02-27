dec4431b2b915a7f6e1d1a8ac1bb054a180c60aa
MitsuakiParis <mitsuaki.vinot@gmail.com>
2018-02-27 17:06:33 +0100

    Complete version of Log In - Log Out - Sign In
    This is the release of the complete version for the creation of a profil. You can sign up by email and then access to your account, after that you can go on other page or log out in this page.
    It takes in count most of common error you can have (bad login info, wrong email type etc...)
    
    Next step, account info displaying, and email verification.

--- public/profil.js
+++ public/profil.js
@@ -1,79 +1,160 @@
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
-document.getElementById("affiche_confirm").style.display = "none"
+
+function afficheProfil(){
+  var user = firebase.auth().currentUser;
+  if (user) {
+    // User is signed in.
+    clearArea();
+    document.getElementById('affiche_connect').style.display = 'none';
+    document.getElementById('affiche_profil').style.display = 'block';
+    var name_dispayer = document.getElementById("player_name")
+    name_dispayer.innerHTML = user.displayName;
+    } else {
+      // No user is signed in.
+      console.log('Grave erreur gros dans la fonction afficheProfil');
+    }
+}
 
 $('input[type=radio]').change(function() {
     var signin = document.getElementById('signin').checked;
-    if (signin == false) {
-        document.getElementById("affiche_confirm").style.display = "block";
-    } else {
-        document.getElementById("affiche_confirm").style.display = "none";
-    }
+    clearArea();
+      if (signin == false) {
+        document.getElementById('error_info').style.display = 'none';
+        document.getElementById('error_message').style.display = 'none';
+        document.getElementById('affiche_confirm').style.display = 'block';
+        document.getElementById('affiche_name').style.display = 'block';
+      } else {
+        document.getElementById('error_info').style.display = 'none';
+        document.getElementById('error_message').style.display = 'none';
+        document.getElementById('affiche_confirm').style.display = 'none';
+        document.getElementById('affiche_name').style.display = 'none';
+      }
 });
 
+document.getElementById('log_out').addEventListener('click', function() {
+  firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+      document.getElementById("signin").checked = true;
+      document.getElementById('error_info').style.display = 'none';
+      document.getElementById('error_message').style.display = 'none';
+      document.getElementById('affiche_confirm').style.display = 'none';
+      document.getElementById('affiche_name').style.display = 'none';
+    }).catch(function(error) {
+      // An error happened.
+    });
+  });
+
 // Button add player listener
-document.getElementById("log_in").addEventListener("click", function() {
+document.getElementById('log_in').addEventListener('click', function() {
     // Clear color inputs
-    $("#ID_input").removeClass("is-invalid");
-    $("#password_input").removeClass("is-invalid");
-    $("#password_confirm_input").removeClass("is-invalid");
+    $('#ID_input').removeClass('is-invalid');
+    $('#password_input').removeClass('is-invalid');
+    $('#password_confirm_input').removeClass('is-invalid');
     // Clear color inputs
-    var id = document.getElementById("ID_input").value;
-    var password = document.getElementById("password_input").value;
-    var password_confirm = document.getElementById("password_confirm_input").value;
+    var email = document.getElementById('ID_input').value;
+    var password = document.getElementById('password_input').value;
+    var password_confirm = document.getElementById('password_confirm_input').value;
     var signin = document.getElementById('signin').checked;
-    console.log(id + password + password_confirm);
-    document.getElementById("log_in").disabled = false;
+    document.getElementById('log_in').disabled = false;
     //SIGN IN
-    if (id != "" && password != "" && signin == true) {
+    if (email != '' && password != '' && signin == true) {
         firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
-            // ...
-        });
-    } else if (id != "" && password != "" && signin == false) { // if inputs are not empty and the password is not the same of password confirm
-        // AUTH ici gros
-        if (password == password_confirm) {
-            firebase.auth().createUserWithEmailAndPassword(id, password).catch(function(error) {
+            $('#ID_input').addClass('is-invalid');
+            $('#password_input').addClass('is-invalid');
+            $('#error_message').removeClass('text-success');
+            $('#error_message').addClass('text-danger');
+            var errortxt = document.getElementById('error_message');
+            document.getElementById('error_info').style.display = 'block';
+            document.getElementById('error_message').style.display = 'block';
+            errortxt.textContent = 'Wrong Email adress or Passeword please try again.';
+        });}
+    else if (email != '' && password != '' && signin == false) {
+    //SIGN UP code is executed when signin radio button isn't checked, and that ID and Password is not empty
+    //The first thing to create an account is to put a name, a mail and a proper password
+          if (password == password_confirm) {
+            firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
                 // Handle Errors here.
                 var errorCode = error.code;
                 var errorMessage = error.message;
-                document.getElementById("error_info").style.display = "block";
-                document.getElementById("error_message").style.display = "block";
-                $("#error_message").removeClass("text-success");
-                $("#error_message").addClass("text-danger");
-                var errortxt = document.getElementById("error_message");
+                document.getElementById('error_info').style.display = 'block';
+                document.getElementById('error_message').style.display = 'block';
+                $('#error_message').removeClass('text-success');
+                $('#error_message').addClass('text-danger');
+                var errortxt = document.getElementById('error_message');
                 errortxt.textContent = errorMessage;
-                console.log(errorCode);
-                if (errorCode == "auth/invalid-email") {
-                    $("#ID_input").addClass("is-invalid");
+                if (errorCode == 'auth/invalid-email') {
+                    $('#ID_input').addClass('is-invalid');
                 }
-                if (errorCode == "auth/weak-password") {
-                    $("#password_input").addClass("is-invalid");
-                    $("#password_confirm_input").addClass("is-invalid");
+                if (errorCode == 'auth/weak-password') {
+                    $('#password_input').addClass('is-invalid');
+                    $('#password_confirm_input').addClass('is-invalid');
                 }
             });
-            document.getElementById("error_info").style.display = "block";
-            document.getElementById("error_message").style.display = "block";
-            $("#error_message").removeClass("text-danger");
-            $("#error_message").addClass("text-success");
-            var errortxt = document.getElementById("error_message");
-            errortxt.textContent = "Successfull profil creation.";
-        } else {
-            document.getElementById("error_info").style.display = "block";
-            document.getElementById("error_message").style.display = "block";
-            var errortxt = document.getElementById("error_message");
-            errortxt.textContent = "Password and Password confirmation must be the same.";
-            $("#password_input").addClass("is-invalid");
-            $("#password_confirm_input").addClass("is-invalid");
         }
-    } else {
-        if (id == "")
-            $("#ID_input").addClass("is-invalid");
-        if (password == "")
-            $("#password_input").addClass("is-invalid");
-        if (password == "")
-            $("#password_confirm_input").addClass("is-invalid");
+        else {
+          document.getElementById('error_info').style.display = 'block';
+          document.getElementById('error_message').style.display = 'block';
+          var errortxt = document.getElementById('error_message');
+          errortxt.textContent = 'Password and Password confirmation must be the same.';
+          $('#password_input').addClass('is-invalid');
+          $('#password_confirm_input').addClass('is-invalid');
+        }}
+    else {
+      if (id == '')
+          $('#ID_input').addClass('is-invalid');
+      if (password == '')
+          $('#password_input').addClass('is-invalid');
+      if (password == '')
+          $('#password_confirm_input').addClass('is-invalid');
     }
 });
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    if(document.getElementById('signin').checked == true){
+      afficheProfil();
+      clearArea();
+    } else{
+      var user = firebase.auth().currentUser;
+      user.updateProfile({
+      displayName: document.getElementById('name_input').value,
+      }).then(function() {
+        document.getElementById('error_info').style.display = 'block';
+        document.getElementById('error_message').style.display = 'block';
+        $('#error_message').removeClass('text-danger');
+        $('#error_message').addClass('text-success');
+        var errortxt = document.getElementById('error_message');
+        errortxt.textContent = 'Successfull profil creation.';
+        afficheProfil()
+
+      }).catch(function(error) {
+        document.getElementById('error_info').style.display = 'block';
+        document.getElementById('error_message').style.display = 'block';
+        $('#error_message').removeClass('text-success');
+        $('#error_message').addClass('text-danger');
+        var errortxt = document.getElementById('error_message');
+        console.log(error.message);
+        errortxt.textContent = error.errorMessage;
+      });
+    }
+  } else {
+    afficheSignin();
+    clearArea();
+  }
+});
+
+function afficheSignin(){
+  document.getElementById('affiche_profil').style.display = 'none';
+  document.getElementById('affiche_connect').style.display = 'block';
+}
+
+function clearArea(){
+  $('#name_input').val('');
+  $('#ID_input').val('');
+  $('#password_input').val('');
+  $('#password_confirm_input').val('');
+}
